d2f28799bb06de4e129467c7f96459e9ebc47207
Vishwas Sharma <vishwassharma332@gmail.com>
2022-07-13 22:38:31 +0530

    main

---
+++ react-movie-app/src/components/AddFavourites.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddFavourite = () => {
+	return (
+		<>
+			<span className='add'>Add to PlayLists</span>
+		</>
+	);
+};
+
+export default AddFavourite;
---
+++ react-movie-app/src/components/RemoveFavourites.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RemoveFavourites = () => {
+	return (
+		<>
+			<span className='add'>Remove from PlayLists</span>
+		
+		</>
+	);
+};
+
+export default RemoveFavourites;
